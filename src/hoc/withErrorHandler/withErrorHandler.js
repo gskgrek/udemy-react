373ed8a163065033a5beab20f1b5da294d3df278
Grzegorz Sieczkowski <g.sieczkowski@albedomarketing.pl>
2019-10-17 15:22:18 +0200

    remove unused interceptors

--- src/hoc/withErrorHandler/withErrorHandler.js
+++ src/hoc/withErrorHandler/withErrorHandler.js
@@ -5,21 +5,26 @@ import Modal from '../../components/UI/Modal/Modal';
 const withErrorHandler = (WrappedComponent, axios) => {
     return class extends Component {
 
-        constructor(props){
-            axios.interceptors.request.use(req => {
-                this.setState({error: null});
-                return req;
-            });
-            axios.interceptors.response.use(res => res, error => {
-                this.setState({error: error});
-            });
-
-            super(props);
-        }
-
         state = {
             error: null,
         };
+
+        constructor(props){
+            super(props);
+
+            this.requestInterceptor = axios.interceptors.request.use(req => {
+                this.setState({error: null});
+                return req;
+            });
+            this.responseInterceptor = axios.interceptors.response.use(res => res, error => {
+                this.setState({error: error});
+            });
+        }
+
+        componentWillUnmount() {
+            axios.interceptors.request.eject(this.requestInterceptor);
+            axios.interceptors.response.eject(this.responseInterceptor);
+        }
 
         errorConfirmedHanlder = () =>{
             this.setState({error: null})
