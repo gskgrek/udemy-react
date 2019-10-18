295db7939dc0ae8b032ac735b2a52e380b4a1487
Grzegorz Sieczkowski <g.sieczkowski@albedomarketing.pl>
2019-10-18 14:10:03 +0200

    checkout summary initial

---
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Burger from '../../Burger/Burger';
+import Button from '../../UI/Button/Button';
+
+import css from './CheckoutSummary.module.scss';
+
+const checkoutSummary = (props) => {
+
+    return(
+        <div className={css.CheckoutSummary}>
+            <h1>We hope it tastes well!</h1>
+            <div style={{width: '100%', margin: 'auto'}}>
+                <Burger ingredients={props.ingredients}/>
+            </div>
+            <Button type="Danger" onClick>CANCEL</Button>
+            <Button type="Success" onClick>CONTINUE</Button>
+        </div>
+    );
+};
+
+export default checkoutSummary;
