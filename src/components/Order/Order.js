23991bc90ddd10a7dbc9e6d9dfc3d0e20e8dd8e4
Grzegorz Sieczkowski <g.sieczkowski@albedomarketing.pl>
2019-10-24 12:13:45 +0200

    outputting the orders

--- src/components/Order/Order.js
+++ src/components/Order/Order.js
@@ -1,15 +1,37 @@
 import React from 'react';
 
 import css from './order.module.scss';
+import BurgerIngredient from "../Burger/BurgerIngredient/BurgerIngredient";
 
 const order = (props) => {
+    const ingredients = [];
+
+    for( let ingredientName in props.ingredients ){
+        ingredients.push( {
+            name: ingredientName,
+            amount: props.ingredients[ingredientName]
+        });
+    }
+
+    const ingredientOutput = ingredients.map( ig => {
+        return <span
+            style={{
+                textTransform: 'capitalize',
+                display: 'inline-block',
+                margin: '0 8px',
+                padding: '5px',
+                border: '1px solid #ccc',
+            }}
+        key={ig.name}>{ig.name} ({ig.amount})</span>
+    });
+
     return (
         <div className={css.Order}>
             <p>
-                Ingredients: Salad (1)
+                Ingredients: {ingredientOutput}
             </p>
             <p>
-                Price: <b>$ 5.45</b>
+                Price: <b>$ {(+props.price).toFixed(2)}</b>
             </p>
         </div>
     );
