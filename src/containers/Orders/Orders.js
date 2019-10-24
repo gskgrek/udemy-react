5b91b6506a7dd3d1df4478ec3c42c4512829742f
Grzegorz Sieczkowski <g.sieczkowski@albedomarketing.pl>
2019-10-24 11:59:29 +0200

    fetching orders

--- src/containers/Orders/Orders.js
+++ src/containers/Orders/Orders.js
@@ -1,8 +1,33 @@
 import React, { Component } from 'react';
+import axios from '../../axios-orders';
 
 import Order from '../../components/Order/Order';
+import withErrorHandler from "../../hoc/withErrorHandler/withErrorHandler";
 
 class Orders extends Component{
+
+    state = {
+        orders: [],
+        loading: true,
+    }
+
+    componentDidMount() {
+        axios.get('/orders.json')
+            .then( response => {
+                const orders = [];
+                for( let id in response.data ){
+                    orders.push( {
+                        id: id,
+                        ...response.data[id]
+                    });
+                }
+                this.setState({loading: false, orders: orders});
+            })
+            .catch( () => {
+                this.setState({loading: false});
+            });
+    }
+
     render (){
         return (
             <div>
@@ -13,4 +38,4 @@ class Orders extends Component{
     }
 }
 
-export default Orders;
+export default withErrorHandler(Orders, axios);
